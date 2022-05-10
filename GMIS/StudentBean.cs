cea2c263543a8ca72ad7f647e78e5abcbf954e20
Shengya Ji <wolfheros123@gmail.com>
2022-05-10 17:17:29 +1000

    restructure the DatabaseController Class

--- GMIS/StudentBean.cs
+++ GMIS/StudentBean.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/**
+ * @auther Shengya Ji
+ * @date 10/05/2022
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +12,15 @@ namespace GMIS
 {
     public class StudentBean
     {
-        public string StudentId { get; set; }
-        public string FirstName { get; set; }
-        public string FamilyName { get; set; }
-        public string Title { get; set; }
-        public string GroupID { get; set; }
-        public string Campus { get; set; }
-        public string phone { get; set; }
-        public string Email { get; set; }
-        public string Category { get; set; }
-        public string PhotoURL { get; set; }
+        public string? StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? FamilyName { get; set; }
+        public string? Title { get; set; }
+        public string? GroupID { get; set; }
+        public string? Campus { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Category { get; set; }
+        public string? PhotoURL { get; set; }
     }
 }
